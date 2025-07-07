c9c3606c6f7238cf58ca0284548bc4e0019cca55
GUSTAVO VILLENEUVE <115963339+Villeneuve0@users.noreply.github.com>
2025-07-07 11:29:17 -0300

    Corrigir imagens do produto para hospedagem

--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -5,10 +5,10 @@ const ProductImages = () => {
   const [currentImage, setCurrentImage] = useState(0);
   
   const images = [
-    "/imagens/produto-1.png",
-    "/imagens/produto-2.png",
-    "/imagens/produto-3.png",
-    "/imagens/produto-4.png"
+    "https://images.pexels.com/photos/4465124/pexels-photo-4465124.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "https://images.pexels.com/photos/4239146/pexels-photo-4239146.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "https://images.pexels.com/photos/5217849/pexels-photo-5217849.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "https://images.pexels.com/photos/4239119/pexels-photo-4239119.jpeg?auto=compress&cs=tinysrgb&w=600"
   ];
 
   const nextImage = () => {
