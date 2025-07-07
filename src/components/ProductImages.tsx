4216f7d28a9fe10fb792411599bec34396ae6227
GUSTAVO VILLENEUVE <115963339+Villeneuve0@users.noreply.github.com>
2025-07-07 11:46:43 -0300

    Corrigir qualidade da imagem

--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -5,10 +5,10 @@ const ProductImages = () => {
   const [currentImage, setCurrentImage] = useState(0);
   
   const images = [
-    "https://i.ibb.co/s9HtQcgZ/image.png",
-    "https://images.pexels.com/photos/4239146/pexels-photo-4239146.jpeg?auto=compress&cs=tinysrgb&w=600",
-    "https://images.pexels.com/photos/5217849/pexels-photo-5217849.jpeg?auto=compress&cs=tinysrgb&w=600",
-    "https://images.pexels.com/photos/4239119/pexels-photo-4239119.jpeg?auto=compress&cs=tinysrgb&w=600"
+    "https://i.ibb.co/s9HtQcgZ/image.png?w=800&h=800&fit=crop&auto=format",
+    "https://images.pexels.com/photos/4239146/pexels-photo-4239146.jpeg?auto=compress&cs=tinysrgb&w=800",
+    "https://images.pexels.com/photos/5217849/pexels-photo-5217849.jpeg?auto=compress&cs=tinysrgb&w=800",
+    "https://images.pexels.com/photos/4239119/pexels-photo-4239119.jpeg?auto=compress&cs=tinysrgb&w=800"
   ];
 
   const nextImage = () => {
