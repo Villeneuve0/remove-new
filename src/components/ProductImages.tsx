57facb1f3645ab2c5816a3d9442ebb4bd46a1423
GUSTAVO VILLENEUVE <115963339+Villeneuve0@users.noreply.github.com>
2025-07-08 12:33:46 -0300

    Atualizar terceira imagem do produto

--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -7,7 +7,7 @@ const ProductImages = () => {
   const images = [
     "https://mercado-clone-remover-shop.vercel.app/lovable-uploads/2d5d64fb-2c3d-4b27-89f3-ffa45513eff7.png",
     "https://mercado-clone-remover-shop.vercel.app/lovable-uploads/34a9d7b9-dedf-4101-b32f-ea445ad6a58b.png",
-    "/imagens/produto-3.png",
+    "https://mercado-clone-remover-shop.vercel.app/lovable-uploads/6d739019-f5a9-4212-a8ca-7881b78e28b2.png",
     "/imagens/produto-4.png"
   ];
 
